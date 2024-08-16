306bfd01f5ba24b35da81553c93cd51b52808081
Martin Joshua Garaza <joshua_garaza@yahoo.com>
2024-08-16 14:53:42 +0800

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -84,6 +84,8 @@
                         class="overflow-y-auto shadow-2xl dropdown-content bg-base-100 text-base-content rounded-t-box rounded-b-box">
                         @can('view-stocks')
                             <li><a href="{{ route('dmd.stk') }}">Stocks</a></li>
+                        @endcan
+                        @can('inventory-viewer')
                             <li><a href="{{ route('dmd.stk.sum') }}">Stocks Summary</a></li>
                         @endcan
                         @can('manage-reorder-levels')
