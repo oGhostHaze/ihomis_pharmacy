a09aad44cedccce76631c12a51b49e2200c8e1f5
Martin Joshua Garaza <57517246+stronze21@users.noreply.github.com>
2025-02-26 10:11:58 +0800

    Update ManualConsumptionGenerator.php

--- app/Http/Livewire/ManualConsumptionGenerator.php
+++ app/Http/Livewire/ManualConsumptionGenerator.php
@@ -415,13 +415,13 @@ class ManualConsumptionGenerator extends Component
         $location_id = auth()->user()->pharm_location_id;
 
         $returns = DB::select("
-            SELECT i.pullout_qty, i.pharm_location_id, s.dmdcomb, s.dmdctr, s.chrgcode, price.acquisition_cost unit_cost, s.retail_price
+            SELECT i.pullout_qty, p.pharm_location_id, s.dmdcomb, s.dmdctr, s.chrgcode, price.acquisition_cost unit_cost, s.retail_price
             FROM pharm_pull_out_items i
             JOIN pharm_pull_outs p ON i.detail_id = p.id
             JOIN pharm_drug_stocks s ON i.stock_id = s.id
             JOIN hdmhdrprice price ON s.dmdprdte = price.dmdprdte
             WHERE i.updated_at BETWEEN '" . $from_date . "' AND '" . $to_date . "'
-                AND i.pharm_location_id = '" . $location_id . "'
+                AND p.pharm_location_id = '" . $location_id . "'
         ");
 
         foreach ($returns as $item) {
