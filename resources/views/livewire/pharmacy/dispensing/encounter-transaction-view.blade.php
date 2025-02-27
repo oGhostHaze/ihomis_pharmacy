fb314822bcd6f9ceb653ee6a0909cb521ef06a61
Martin Joshua Garaza <57517246+stronze21@users.noreply.github.com>
2025-02-28 00:59:16 +0800

    Update encounter-transaction-view.blade.php

--- resources/views/livewire/pharmacy/dispensing/encounter-transaction-view.blade.php
+++ resources/views/livewire/pharmacy/dispensing/encounter-transaction-view.blade.php
@@ -1129,8 +1129,8 @@
             Swal.fire({
                 html: `
         <div class="text-xl font-bold"> Deactivate ` + drug + `</div>
-        <div class="flex w-full space-x-3 bg-slate-300 p=6 px-2 mt-2">
-            <input type="text" class="w-full input-border bg-slate-300 input-text input" id="adttl_remarks" />
+        <div class="flex w-full space-x-3 bg-slate-300 p=6 px-2 mt-2 form-control">
+            <input type="text" class="w-full input-border bg-slate-300 input" id="adttl_remarks" />
         </div>
         `,
                 showCancelButton: true,
