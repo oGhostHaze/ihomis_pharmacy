30fa086b35dfaf82517ae9317715cf21060a2fb8
Martin Joshua Garaza <57517246+stronze21@users.noreply.github.com>
2025-05-07 09:36:32 +0800

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -45,7 +45,8 @@ class AuthServiceProvider extends ServiceProvider
                 'DRUMAI',
                 'DRUMAJ',
                 'DRUMAK',
-                'DRUMAL'
+                'DRUMAL',
+                'DRUMAM',
             );
         });
 
